87cc788c9a9fc9132ece55e0ca700bc7a27ba43e
kuwilson2001 <kuwilson0922@gmail.com>
2023-10-17 10:41:10 +0800

    Add Computer Player

---
+++ unity_UI/UI_template/Assets/scripts/Gaming/ComputerPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerPlayer : MonoBehaviour
+{
+    public CardDatabase cardDatabase;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayRandomCard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void PlayRandomCard()
+    {
+        if (cardDatabase != null && cardDatabase.cardList.Count > 0)
+        {
+            int randomIndex = Random.Range(0, cardDatabase.cardList.Count);
+            Card randomCard = cardDatabase.cardList[randomIndex];
+
+            // 出牌
+        }
+    }
+}
